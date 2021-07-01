b856d1d06a6fc74bd0d1e2f6f6be2dae197005cf
Petr Hnyk <hnyk.petr@centrum.cz>
2021-07-01 11:38:35 +0200

    Added Service Worker, App Manifest and new Icons

--- src/main.js
+++ src/main.js
@@ -24,7 +24,8 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-import './registerServiceWorker'
+/* PWA */
+import './registerServiceWorker';
 
 const app = createApp(App)
     .use(IonicVue)
